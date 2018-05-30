ca4e1078982fcf45772cef104234970aa2004a22
Kate Dubitski <kate.dubitski@gmail.com>
2018-05-30 17:28:55 -0400

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'You dont know JS' },
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Eloquent JavaScript' },
+    { title: 'Eloquent Ruby' },
+    { title: 'Javascript and jQuery' },
+  ];
+}
